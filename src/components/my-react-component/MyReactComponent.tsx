b8acd6c11240b977b4b201c7a072cd1b37d2e6ea
Madhulika Mukherjee <madhulika.m@accolite.com>
2022-11-02 16:18:41 +0530

    first commit

--- src/components/my-react-component/MyReactComponent.tsx
+++ src/components/my-react-component/MyReactComponent.tsx
@@ -2,11 +2,47 @@ import * as React from 'react';
 import { FunctionComponent, useEffect, useRef, useState } from 'react';
 
 import './MyReactComponent.scss';
+import * as ReactDOM from 'react-dom';
+import * as FusionCharts from 'fusioncharts';
+import * as Charts from 'fusioncharts/fusioncharts.charts';
+import * as FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
+import ReactFC from 'react-fusioncharts';
+
+ReactFC.fcRoot(FusionCharts, Charts, FusionTheme);
 
 export interface IMyComponentProps {
   counter: number;
   onClick?: () => void;
 }
+
+const dataSource = {
+  chart: {
+    caption: 'Countries With Most Oil Reserves [2017-18]',
+    subCaption: 'In MMbbl = One Million barrels',
+    xAxisName: 'Country',
+    yAxisName: 'Reserves (MMbbl)',
+    numberSuffix: 'K',
+    theme: 'fusion'
+  },
+  data: [
+    { label: 'Venezuela', value: '290' },
+    { label: 'Saudi', value: '260' },
+    { label: 'Canada', value: '180' },
+    { label: 'Iran', value: '140' },
+    { label: 'Russia', value: '115' },
+    { label: 'UAE', value: '100' },
+    { label: 'US', value: '30' },
+    { label: 'China', value: '30' }
+  ]
+};
+
+const chartConfigs = {
+  type: 'column2d',
+  width: 600,
+  height: 400,
+  dataFormat: 'json',
+  dataSource: dataSource
+};
 
 export const MyReactComponent: FunctionComponent<IMyComponentProps> = (props: IMyComponentProps) => {
 
@@ -40,5 +76,6 @@ export const MyReactComponent: FunctionComponent<IMyComponentProps> = (props: IM
             className={'increase-button'}>click to increase</span>
     </div>
     <div className={'comp-state'}>State counter: {stateCounter}</div>
+    <ReactFC {...chartConfigs} />
   </div>;
 };
